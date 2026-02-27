a6deff7e64f31370a7812d1a7dbddfdf7a50151e
Utkarsh Srivastava <utkarshsrivastava964@gmail.com>
2026-02-27 23:27:44 +0530

    server

--- Backend/index.js
+++ Backend/index.js
@@ -1,1 +1,9 @@
-console.log("Work With Mybook");
+import express from express
+import dotenv from "dotenv";
+dotenv.config();
+
+const app =express()
+const PORT = process.env.PORT || 5000;
+app.listen(PORT,()=>{
+    console.log("Server is running.........")
+})
